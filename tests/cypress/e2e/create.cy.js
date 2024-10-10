7d6db860ba700d987a142d1eb609e16a89d2a7b2
Vinicius Oliveira <viniciusoliveira2807@hotmail.com>
2024-10-10 13:14:00 -0300

    creating duplicate food truck script and required fields

--- tests/cypress/e2e/create.cy.js
+++ tests/cypress/e2e/create.cy.js
@@ -28,4 +28,55 @@ describe('Recommendation', () => {
         createPage.submit()
         createPage.modal.haveText('Food truck cadastrado com sucesso!')
     })
+
+    it('should not register a food truck with duplicate name', () => {
+        const user = {
+            name: 'Isabela',
+            instagram: '@isa',
+            password: 'Aaaaaa1!'
+        }
+
+        const foodtruck = {
+            latitude: '-23.633535849219005',
+            longitude: '-46.70527338981629',
+            name: 'Pastel do Alan',
+            details: 'O melhor pastel de São Paulo',
+            opening_hours: 'das 8h as 23h',
+            open_on_weekends: false
+        }
+
+        cy.apiCreateUser(user)
+        cy.apiLogin(user)
+        cy.apiCreateFoodTruck(foodtruck)
+        cy.uiLogin(user)
+
+        cy.visit('/map')
+
+        mapPage.createLink()
+        createPage.form(foodtruck)
+        createPage.submit()
+        createPage.modal.haveText('Esse food truck já foi cadastrado!')
+    })
+
+    it('all fields are required', () => {
+        const user = {
+            name: 'Albert',
+            instagram: '@albert',
+            password: 'Aaaaaa1!'
+        }
+
+        const foodtruck = {
+            latitude: '-23.633535849219005',
+            longitude: '-46.70527338981629',
+        }
+
+        cy.apiCreateUser(user)
+        cy.uiLogin(user)
+
+        mapPage.createLink()
+        cy.setGeolocation(foodtruck.latitude, foodtruck.longitude)
+        createPage.submit()
+        const message = 'Os campos nome, descrição e horário de funcionamento devem ser informados para recomendar um food truck!'
+        createPage.modal.haveText(message)
+    })
 })
